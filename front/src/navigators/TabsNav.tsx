225bb4a58a525f545d8db6f357940bf94d8c87af
jordan-choi <yyoungbest@gmail.com>
2021-08-14 21:08:33 +0900

    feat: tab bar icons

--- front/src/navigators/TabsNav.tsx
+++ front/src/navigators/TabsNav.tsx
@@ -1,13 +1,34 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import SharedStackNav from './SharedStackNav';
+import { Ionicons } from '@expo/vector-icons';
+import Home from '../screens/Home';
+import Community from '../screens/Community';
 
-const Tabs = createBottomTabNavigator();
+
+const Tab = createBottomTabNavigator();
 
 export default function TabsNav() {
     return (
-        <Tabs.Navigator>
-            <Tabs.Screen name="Home">{() => <SharedStackNav screenName="Home" />}</Tabs.Screen>
-        </Tabs.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if(route.name === "Home"){
+                        iconName = focused 
+                            ? 'ios-home'
+                            : 'ios-home-outline';
+                    } else if(route.name === "Community"){
+                        iconName = focused 
+                            ? 'ios-chatbubbles' 
+                            : 'ios-chatbubbles-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />
+                }
+            })}
+        >
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Community" component={Community} />
+        </Tab.Navigator>
     );
 }
