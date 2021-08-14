6153995aa96d5ceb8f458a3e5b9ec68d702018d8
inthewalter <inwalter99@gmail.com>
2021-08-14 20:04:35 +0900

    feat: 임시 Text 컴포넌트 사용

---
+++ front/src/components/commons/Text.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const GrayText = styled.Text`
+    font-size: 12px;
+    color: #bdbdbd;
+`;
+export const StrongText = styled.Text`
+    font-weight: 700;
+    font-size: 16px;
+`;
+export const HighlightText = styled.Text`
+    color: #0acf20;
+`;
